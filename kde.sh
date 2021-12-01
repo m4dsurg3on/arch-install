27dbb7f40ba590f4ed5c258caa8eb3d47e54d141
m4dsurg3on <49027255+m4dsurg3on@users.noreply.github.com>
2021-12-01 08:59:13 +0000

    Create kde.sh

---
+++ kde.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+
+# Optional ZRAMD package for the SWAP https://wiki.archlinux.org/title/Improving_performance#zram_or_zswap
+# yay -S zramd
+
+sudo reflector -c Czechia -a 6 --sort rate --save /etc/pacman.d/mirrorlist
+sudo pacman -Syy
+
+sudo pacman -S --noconfirm xorg sddm plasma kde-applications firefox libreoffice vlc papirus-icon-theme materia-kde
+
+sudo systemctl enable sddm
+# sudo systemctl enable --now zramd.service
